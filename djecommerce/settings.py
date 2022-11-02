7955e66203a118d03e2940557e4f5ddf173bbc9a
Mace Admin <macetechdev@gmail.com>
2022-11-02 10:10:39 +0100

    m

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-tlf8(@5zp0cu+o3-z!9po%f=w%5p$%dh09i2owhjvn7saoh@g7
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','smokisess.herokuapp.com','www.smokisess.herokuapp.com','smokisess.com','www.smokisess.com']
-
+SECURE_SSL_REDIRECT = True
 
 # Application definition
 
